e177af971e619227be69b248ba5f6ae7ecb7a92c
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-10-31 21:57:20 -0500

    first commit

---
+++ src/main/java/eci/edu/dows/taller/Taller_Master_Chef/Taller/Master/Chef/TallerMasterChefApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.dows.taller.Taller_Master_Chef.Taller.Master.Chef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerMasterChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerMasterChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/dows/taller/Taller_Master_Chef/Taller/Master/Chef/TallerMasterChefApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dows.taller.Taller_Master_Chef.Taller.Master.Chef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMasterChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
